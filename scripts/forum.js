bbb60257fd34d3d9e9abc2f5f21ba45e381f5110
SchuyAW <ssaw@live.co.uk>
2024-08-10 10:32:03 +0100

    Update forum.js to use secrets

--- scripts/forum.js
+++ scripts/forum.js
@@ -4,13 +4,13 @@ import { getFirestore, collection, addDoc, query, orderBy, getDocs, serverTimest
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: window.env.FIREBASE_API_KEY,
-    authDomain: window.env.FIREBASE_AUTH_DOMAIN,
-    projectId: window.env.FIREBASE_PROJECT_ID,
-    storageBucket: window.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: window.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: window.env.FIREBASE_APP_ID,
-    measurementId: window.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "YOUR_FIREBASE_API_KEY",
+    authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
+    projectId: "YOUR_FIREBASE_PROJECT_ID",
+    storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_FIREBASE_MESSAGING_SENDER_ID",
+    appId: "YOUR_FIREBASE_APP_ID",
+    measurementId: "YOUR_FIREBASE_MEASUREMENT_ID"
 };
 
 // Initialize Firebase
